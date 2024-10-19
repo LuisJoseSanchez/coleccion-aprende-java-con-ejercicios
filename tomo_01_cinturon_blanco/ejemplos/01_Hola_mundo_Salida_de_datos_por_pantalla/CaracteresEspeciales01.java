bacf21eb2133869acd40da88f4abab4bf9a24d1b
Luis José Sánchez <luisjoseprofe@gmail.com>
2024-10-20 01:07:14 +0200

    Ejemplos actualizados

---
+++ tomo_01_cinturon_blanco/ejemplos/01_Hola_mundo_Salida_de_datos_por_pantalla/CaracteresEspeciales01.java
@@ -0,0 +1,9 @@
+/**
+ * Muestra caracteres especiales
+ */
+public class CaracteresEspeciales01 {
+  public static void main(String[] args) {
+    System.out.print("Cuando programo ⌨ estoy");
+    System.out.println(" muy ☺ y me siento como un ♔");
+  }
+}
