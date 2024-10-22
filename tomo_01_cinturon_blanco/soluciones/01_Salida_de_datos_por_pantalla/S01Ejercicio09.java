63b60f248f9f7a5d25e851ddc5a436fae7b7cb15
Luis José Sánchez <luisjoseprofe@gmail.com>
2024-10-22 13:01:39 +0200

    Actualizando soluciones

--- tomo_01_cinturon_blanco/soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio09.java
+++ tomo_01_cinturon_blanco/soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio09.java
@@ -3,15 +3,17 @@
  */
 public class S01Ejercicio09 {
   public static void main(String[] args) {
-    System.out.println("· · · · · · · · · · · ·  ●●● · · · ·");
-    System.out.println("· · APRENDE JAVA· · · · · ●●●· · · ·");
-    System.out.println("· ·CON EJERCICIOS · · · · ●● · · · ·");
-    System.out.println("· Luis José Sánchez · ████████████ ·");
-    System.out.println("· · · · · · · · · · · ████████ · ██ ·");
-    System.out.println("· · · · · · · · · · · ████████████ ·");
-    System.out.println("· · · · · · · · · · · ████████ · · ·");
-    System.out.println("░░░░░░░░░░░░░░░░░░░░░░████████░░░░░░");
-    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+    System.out.println("· · · · · · · · · · · · · · · · · · · ·");
+    System.out.println("· · · · · · · · · · · · ·  ●●●  · · · ·");
+    System.out.println("· · · APRENDE JAVA· · · · · ●●● · · · ·");
+    System.out.println("· · ·CON EJERCICIOS · · · · ●●  · · · ·");
+    System.out.println("· · · · · · · · · · · · · · · · · · · ·");
+    System.out.println("· ·Luis José Sánchez· ███████████████ ·");
+    System.out.println("· · · · · · · · · · · ██████████ · ██ ·");
+    System.out.println("· · · · · · · · · · · ███████████████ ·");
+    System.out.println("· · · · · · · · · · · ██████████  · · ·");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░██████████░░░░░░░");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
   }
 }
--- tomo_01_cinturon_blanco/soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio10.java
+++ tomo_01_cinturon_blanco/soluciones/01_Salida_de_datos_por_pantalla/S01Ejercicio10.java
@@ -3,17 +3,23 @@
  */
 public class S01Ejercicio10 {
   public static void main(String[] args) {
-    System.out.println("\033[31m");
-    System.out.println("                        ██");
-    System.out.println("    APRENDE JAVA      ██");
-    System.out.println("   CON EJERCICIOS       ██\n\033[33m");
-    System.out.println("  Luis José Sánchez \033[34m ████████████");
-    System.out.println("                     ████████  ██");
-    System.out.println("                     ████████████");
-    System.out.println("                     ████████");
-    System.out.println("░░░░░░░░░░░░░░░░░░░░░████████░░░░░░░░");
-    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
-    System.out.println("\033[37m"); // volvemos al blanco
+    System.out.println("\033[34m");
+    System.out.println("· · · · · · · · · · · · · · · · · · · ·");
+    System.out.print("· · · · · · · · · · · · ·  \033[90m●●●");
+    System.out.println("\033[96m  · · · ·");
+    System.out.print("· · · \033[31mAPRENDE JAVA\033[96m· · · · · ");
+    System.out.println("\033[90m●●●\033[96m · · · ·");
+    System.out.print("· · ·\033[31mCON EJERCICIOS");
+    System.out.println("\033[96m · · · · \033[90m●●\033[96m  · · · ·");
+    System.out.println("\033[96m· · · · · · · · · · · · · · · · · · · ·");
+    System.out.print("· ·\033[93mLuis José Sánchez\033[34m· ");
+    System.out.println("███████████████ ·");
+    System.out.println("· · · · · · · · · · · ██████████ · ██ ·");
+    System.out.println("· · · · · · · · · · · ███████████████ ·");
+    System.out.println("· · · · · · · · · · · ██████████  · · ·");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░██████████░░░░░░░");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+    System.out.println("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+    System.out.println("\033[37m");
   }
 }
